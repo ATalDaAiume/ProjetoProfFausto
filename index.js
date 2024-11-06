2c738dbac9e6c63fe93a303acca42324cad39c86
Luiz Felipe <luizfelipe01700@gmail.com>
2024-11-06 19:30:47 -0300

    adicionei duas rotas e as partiasl nas paginas

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const app = express();
 
 app.set("view engine", "php");
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/pg2", (req, res) => {
+    res.render("pg2");
+});
+
+app.listen(3030, (req, res) => {
+    console.log("Server on");
+});
