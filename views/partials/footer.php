4fbfcfdd05179ffed2577c287d19beb9574f6291
miguel <miguelantoniomg2005@gmail.com>
2024-11-06 21:40:54 -0300

    footerAdicionado:)

---
+++ views/partials/footer.php
@@ -0,0 +1,46 @@
+<footer>
+    <h2>Inscrições abertas</h2>
+    <br>
+    <ul class="container_quadrados">
+        <li class="quadrado">
+            <p>CURSO LIVRE</p><br>
+            <p>
+                Atualização no Tratamento de Feridas e Curativos<br>
+                Inicio 07/02/2025
+            </p><br>
+            <p>Senac Rio do Sul</p>
+        </li>
+        <li class="quadrado">
+            <p>CURSO TÉCNICO</p><br>
+            <p>
+                Técnico em Enfermagem<br>
+                Inicio 10/02/2025.
+            </p><br>
+            <p>Senac Brusque</p>
+        </li>
+        <li class="quadrado">
+            <p>GRADUAÇÃO</p><br>
+            <p>
+                Superior de Tecnologia em Processos Gerenciais<br>
+                Inicio 10/02/2025
+            </p><br>
+            <p>Senac Joinville</p>
+        </li>
+        <li class="quadrado">
+            <p>PÓS-GRADUAÇÃO</p><br>
+            <p>
+                Pós-Graduação Lato Sensu em Marketing Digital<br>
+                Inicio 08/11/2024
+            </p><br>
+            <p>Senac Concordia</p>
+        </li>
+        <li class="quadrado">
+            <p>ESPECIALIZAÇÃO TÉCNICA</p><br>
+            <p>
+                Especialização Técnica de Enfermagem em Saúde Mental<br>
+                Inicio 06/03/2025
+            </p><br>
+            <p>Senac Florianópolis</p>
+        </li>
+    </ul>
+</footer>
